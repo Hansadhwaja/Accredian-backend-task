8d43c3f2607c693c1a2d8ae7ae6611f82ee2235b
Hansadhwaja Biswal <hansadhwajabiswal@gmail.com>
2024-07-19 12:33:10 +0530

    added postgress sql db

---
+++ prisma/migrations/20240719065935_migration/migration.sql
@@ -0,0 +1,13 @@
+-- CreateTable
+CREATE TABLE "Referral" (
+    "id" SERIAL NOT NULL,
+    "referrerName" TEXT NOT NULL,
+    "referrerEmail" TEXT NOT NULL,
+    "refereeName" TEXT NOT NULL,
+    "refereeEmail" TEXT NOT NULL,
+    "course" TEXT NOT NULL,
+    "comments" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Referral_pkey" PRIMARY KEY ("id")
+);
